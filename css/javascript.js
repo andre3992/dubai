4905ddd2584ce3ff21d09564be4268b0b00acae2
andre3992 <andre.regedor@hotmail.com>
2019-11-23 11:59:39 +0000

    changes to modal

--- css/javascript.js
+++ css/javascript.js
@@ -2,6 +2,8 @@ const openModalElement = document.getElementById('open-modal');
 const openModalElementFooter = document.getElementById('open-modalfooter');
 const modalElement = document.getElementById('modal');
 const closeModalElement = document.getElementById('close-modal');
+const closeModalElementX = document.getElementById('close-modalx');
+
 
 
 function bindEvents(element, displayProperty) {
@@ -13,3 +15,4 @@ function bindEvents(element, displayProperty) {
 bindEvents(openModalElement, 'block');
 bindEvents(openModalElementFooter, 'block');
 bindEvents(closeModalElement, 'none');
+bindEvents(closeModalElementX, 'none');
